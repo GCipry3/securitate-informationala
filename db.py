1bf30cad0352e5ffe20b97a9df3c5e0882b55712
cgalbeaza <galbeazaciprian30@gmail.com>
2024-03-27 12:39:44 +0200

    Add env and requirements.txt

--- db.py
+++ db.py
@@ -1,5 +1,8 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-MONGODB_URL = "mongodb://root:rootpassword@localhost:27017/encryption_db"
+MONGODB_URL = os.environ.get("MONGO_URI",None)
 client = MongoClient(MONGODB_URL)
 db = client["encryption_db"]
